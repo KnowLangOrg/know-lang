02ad63982a9dbde945bdf0e1613ff9ed3aa980f0
gaby <kimgb415@gmail.com>
2025-06-12 17:50:49 +0900

    remove depcreated hf demo script

--- hf_transformers_demo.py
+++
@@ -1,6 +0,0 @@
-from knowlang.chat_bot.chat_interface import create_chatbot
-from knowlang.configs import AppConfig
-
-config = AppConfig()
-demo = create_chatbot(config)
-demo.launch(server_name="0.0.0.0", server_port=7860)
