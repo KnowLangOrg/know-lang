f0969dcd35002f52f94149e14cbce98977bf8610
gaby <kimgb415@gmail.com>
2025-06-18 11:06:41 +0900

    make parse command async

--- knowlang/api/parse/router.py
+++ knowlang/api/parse/router.py
@@ -7,21 +7,11 @@ from fastapi import APIRouter, BackgroundTasks
 LOG = FancyLogger(__name__)
 router = APIRouter()
 
-
-# https://stackoverflow.com/questions/71516140/fastapi-runs-api-calls-in-serial-instead-of-parallel-fashion
-# https://stackoverflow.com/questions/67599119/fastapi-asynchronous-background-tasks-blocks-other-requests
-# very hacky workaround to allow parse command to run in the background
-async def parse_command_hack(args: ParseCommandArgs):
-    from asyncio import sleep
-    await sleep(1)  # Simulate a small delay to yield control to the event loop for response
-
-    await parse_command(args)
-
 @router.post("/parse")
 async def parse_command_endpoint(args: ParseCommandArgs, background_tasks: BackgroundTasks):
     LOG.info(f"Received parse command with args: {args}")
     
-    background_tasks.add_task(parse_command_hack, args=args)
+    background_tasks.add_task(parse_command, args=args)
     
     return JSONResponse(
         content={"status": "success", "message": "Parse command triggered successfully."},
