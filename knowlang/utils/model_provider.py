ae465e401819515133dbbfca6bad746610a4bdea
gaby <kimgb415@gmail.com>
2025-07-13 19:47:56 +0900

    knowlang llm provider (openai compatible)

--- knowlang/utils/model_provider.py
+++ knowlang/utils/model_provider.py
@@ -1,11 +1,13 @@
 from pydantic_ai.models import Model, KnownModelName
 from knowlang.core.types import ModelProvider
 from typing import get_args
+from knowlang.configs.config import LLMConfig
 
 def create_pydantic_model(
-    model_provider: ModelProvider,
-    model_name: str,
+    config: LLMConfig,
 ) -> Model | KnownModelName:
+    model_provider = config.model_provider
+    model_name = config.model_name
     model_str = f"{model_provider}:{model_name}"
 
     known_model_names = get_args(KnownModelName.__value__)
@@ -26,5 +28,19 @@ def create_pydantic_model(
             model_name=model_name,
             provider=OpenAIProvider(base_url="http://localhost:11434/v1"),
         )
+    elif model_provider == ModelProvider.KNOWLANG:
+        from pydantic_ai.models.openai import OpenAIModel
+        from pydantic_ai.providers.openai import OpenAIProvider
+        base_url = config.model_settings.get("base_url", None)
+        if not base_url:
+            raise ValueError(
+                "KnowLang model provider does not support base_url in model settings."
+            )
+
+        return OpenAIModel(
+            model_name=model_name,
+            provider=OpenAIProvider(base_url=base_url),
+        )
+
     else:
         raise NotImplementedError(f"Model {model_provider}:{model_name} is not supported")
